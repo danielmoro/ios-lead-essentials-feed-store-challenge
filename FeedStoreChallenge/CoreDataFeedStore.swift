8511881da39a6856a83faaa7b84978d0f568a041
danielmoro <daniel.moro@me.com>
2021-03-23 15:43:00 +0100

    Move `CoreDataFeedStore` to its own file in the production in preparation to add `CoreData` model

---
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -0,0 +1,23 @@
+//
+//  Created by Daniel Moro on 23.3.21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//  
+
+import Foundation
+
+public class CoreDataFeedStore: FeedStore {
+	public init() {
+		
+	}
+	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+		
+	}
+	
+	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+		
+	}
+	
+	public func retrieve(completion: @escaping RetrievalCompletion) {
+		completion(.empty)
+	}
+}
