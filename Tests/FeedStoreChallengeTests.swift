473decf81a78aa2d366d1c740057bfe57205f690
danielmoro <daniel.moro@me.com>
2021-03-23 15:39:40 +0100

    `CoreDataFeedStore` returns empty result if cache is empty

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -15,7 +15,7 @@ class CoreDataFeedStore: FeedStore {
 	}
 	
 	func retrieve(completion: @escaping RetrievalCompletion) {
-		
+		completion(.empty)
 	}
 }
 
@@ -34,9 +34,9 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
 	//  ***********************
 	
 	func test_retrieve_deliversEmptyOnEmptyCache() throws {
-//		let sut = try makeSUT()
-//		
-//		assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
+		let sut = try makeSUT()
+		
+		assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
 	}
 	
 	func test_retrieve_hasNoSideEffectsOnEmptyCache() throws {
